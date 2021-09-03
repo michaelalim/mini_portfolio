4dd9560dd1a9fd297b732e0b3168339cd325300e
Michael Jason Alim <lolmichael03@gmail.com>
2021-09-03 15:34:06 +0800

    added spinner
    added loading spinner made using css

---
+++ Projects/projectArray.js
@@ -0,0 +1,46 @@
+export const projectArr = [
+    {projectName: "Energy Meter", 
+    link: "easy/energyMeter/index.html", 
+    screen:"easy/energyMeter/energyMeter.gif", 
+    dif:"eas", ref: "scr", lang: "js"},
+
+    {projectName: "Name Beautifier", 
+    link: "easy/nameBeautifier/index.html", 
+    screen:"easy/nameBeautifier/nameBeautifier.gif", 
+    dif:"eas", ref: "scr", lang: "js"},
+
+    {projectName: "Social Media for Cats", 
+    link: "easy/socialMediaForCats/index.html", 
+    screen:"easy/socialMediaForCats/socialMediaForCats.gif", 
+    dif:"eas", ref: "scr", lang: "js"},
+
+    {projectName: "Transparent Login Form", 
+    link: "easy/basicLoginScreen/index.html", 
+    screen:"easy/basicLoginScreen/loginForm.gif", 
+    dif:"eas", ref: "oth", lang: "css"},
+    
+    {projectName: "CSS-Made Emoji & Tooltip", 
+    link: "easy/cssTooltipsEmoji/index.html", 
+    screen:"easy/cssTooltipsEmoji/cssTooltips.gif", 
+    dif:"eas", ref: "oth", lang: "css"},
+
+    {projectName: "Magic 8 Ball", 
+    link: "moderate/magicBall/index.html", 
+    screen:"moderate/magicBall/magicBall.gif", 
+    dif:"mod", ref: "scr", lang: "js"},
+
+    {projectName: "Button Animation", 
+    link: "moderate/buttonAnimation/index.html", 
+    screen:"moderate/buttonAnimation/buttonAnimation.gif", 
+    dif:"mod", ref: "oth", lang: "css"},
+
+    {projectName: "3D Button Animation", 
+    link: "moderate/button3dAnimation/index.html", 
+    screen:"moderate/button3dAnimation/button3dAnimation.gif", 
+    dif:"mod", ref: "oth", lang: "css"},
+
+    {projectName: "CSS Loading Spinner", 
+    link: "easy/spinner/index.html", 
+    screen:"easy/spinner/spinner.gif", 
+    dif:"eas", ref: "oth", lang: "css"}
+];
